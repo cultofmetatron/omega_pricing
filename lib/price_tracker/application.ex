c7944ff4d1c752c6d4f533d0001bb3255398e822
Peter de Croos <cultofmetatron@aumlogic.com>
2017-02-23 20:18:40 -0500

    initial commit

---
+++ lib/price_tracker/application.ex
@@ -0,0 +1,22 @@
+defmodule PriceTracker.Application do
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Starts a worker by calling: PriceTracker.Worker.start_link(arg1, arg2, arg3)
+      # worker(PriceTracker.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: PriceTracker.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ test/price_tracker_test.exs
@@ -0,0 +1,8 @@
+defmodule PriceTrackerTest do
+  use ExUnit.Case
+  doctest PriceTracker
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
