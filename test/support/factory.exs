b17064141cd1fab82bdc378218380f16269df533
Peter de Croos <cultofmetatron@aumlogic.com>
2017-02-25 21:19:49 -0500

    added factories

--- test/support/factory.exs
+++ test/support/factory.exs
@@ -4,13 +4,25 @@ defmodule PriceTracker.Factory do
 
   # without Ecto
   use ExMachina
-
+  
+  alias PriceTracker.Product
+  alias PriceTracker.PastProductRecord
+  
   def product_factory do
+    %Product{
+      company_code: sequence(:product_name, &"ACME_#{&1}"),
+      product_name: sequence(:product_name, &"chair_#{&1}"),
+      external_product_id: sequence(:external_product_id, &"#{&1}"),
+      price: 2000
+    }
   end
 
   def past_price_record_factory do
+    %PastPriceRecord{
+      price: sequence(:price, &(&1))
+      percentage_change: 0,
+      build(:product)
+    }
   end
 
-  def comment_factory do
-  end
 end
