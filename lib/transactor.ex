95766a1b5343c2ea305166d3252165d3b830b2a1
Peter de Croos <cultofmetatron@aumlogic.com>
2017-02-26 18:02:06 -0500

    added test for updated

--- lib/transactor.ex
+++ lib/transactor.ex
@@ -12,13 +12,13 @@ defmodule PriceTracker.Transactor do
     
   end
 
-  def merge_product(product, repo) do
+  def merge_product(params, repo) do
     # find existing product with code and external product id
-    case find_existing_product(product) |> repo.one() do
+    case find_existing_product(params) |> repo.one() do
       nil ->
-        create_product(product, repo)
+        create_product(params, repo)
       %Product{}=product ->
-        update_product(product, repo)
+        update_product(product, params, repo)
     end
   end
 
@@ -27,9 +27,9 @@ defmodule PriceTracker.Transactor do
     the system.
 
   """
-  defp create_product(product, repo) do
+  defp create_product(params, repo) do
     repo.transaction(fn() ->
-      new_product = Product.changeset(%Product{}, product)
+      new_product = Product.changeset(%Product{}, params)
                     |> repo.insert!()
                     |> repo.preload([:past_price_records])
       new_past_price_record = build_assoc(new_product, :past_price_records)
@@ -42,9 +42,48 @@ defmodule PriceTracker.Transactor do
     end)
   end
 
-  defp update_product(product, repo) do
-    {:error, :not_implimented}
+  @docp """
+    Takes a product and repo and updates the product in
+    the system if the name has not changed
+
+  """
+  defp update_product(product, params, repo) do
+    #{:error, :not_implimented}
+    if name_changed?(product, params) do
+      {:error, :name_for_product_changed}
+    else
+      repo.transaction(fn() ->
+        # get the most recent price record
+        product = product |> repo.preload([:past_price_records])
+        # there's prolly a way to do this in the db but its a premature opt
+        # at this point
+        latest_record = product.past_price_records
+          |> Enum.sort_by(fn(record) -> record.inserted_at end, &>=/2)
+          |> Enum.at(0)
+
+        # if this gets triggerred, something has messed with the database
+        # in a way that voids the assumptions we make about the state
+        # of the database. In such a case, it is prudent to raise an error
+        # and stop operation till an engineer looks into it.
+        if (latest_record.price !== product.price) do
+          raise RuntimeError, message: "price record does not match current product"
+        end
+
+        #update the price
+        product = Product.changeset(product, %{price: params.price}) |> repo.update!()
+        build_assoc(product, :past_price_records)
+          |> PastPriceRecord.changeset(%{
+               price: product.price,
+               percentage_change: (params.price - latest_record.price)/latest_record.price
+             })
+          |> repo.insert!()
+        product |> repo.preload([:past_price_records], force: true)
+      end)
+    end
   end
+
+  defp price_changed?(product, params), do: product.price != params.price
+  defp name_changed?(product, params),  do: product.product_name != params.product_name
 
   defp find_existing_product(product) do
     %{company_code: code, external_product_id: external_id} = product
--- test/01_transactor/00_transactor_test.exs
+++ test/01_transactor/00_transactor_test.exs
@@ -14,9 +14,11 @@ defmodule PriceTracker.TransactorTest do
 
   describe "Transactor.merge_product" do
     @docp """
-      insert one product that doesn't exist should
-      return a tuple {:ok, product } and we should be able t check the db
-      for that product
+      [x] If you do not have a product with that external_product_id and the product is
+          not discontinued, create a new product record for it.
+      
+      [ ] Explicitly log that there is a new product and that you are creating
+          a new product.
     """
     test "inserts the product into the db and creates a price record when the product does not exist" do
       {:ok, product } = Transactor.merge_product(
@@ -27,43 +29,69 @@ defmodule PriceTracker.TransactorTest do
           price: 50000
         }, Repo)
       
-      stored_product = Repo.get(Product, Map.get(product, :id), preload: [:past_price_logs])
-      #|> Repo.preload(:past_price_logs)
+      stored_product = Repo.get(Product, Map.get(product, :id))
+        |> Repo.preload(:past_price_records)
       assert stored_product != nil
+      assert stored_product.company_code == product.company_code
+      assert Enum.count(stored_product.past_price_records) == 1
     end
 
     @docp """
-      If you do not have a product with that external_product_id and the product is 
-      not discontinued, create a new product record for it. Explicitly log that there 
-      is a new product and that you are creating a new product.
+      [x] if you have a product record with a matching external_product_id
+          but a different product name, log an error message that warns the team that
+          there is a mismatch. Do not update the price.
     """
-    test "if the product does not exist, it creates the product and pricelog" do
+    test "returns error if the name has changed for the product" do
+      {:ok, product } = Transactor.merge_product(
+        %{
+          company_code: "ACME",
+          external_product_id: "5323",
+          product_name: "acme chair no 5",
+          price: 50000
+        }, Repo)
 
+      assert {:error, :name_for_product_changed } = Transactor.merge_product(
+        %{
+          company_code: "ACME",
+          external_product_id: "5323",
+          product_name: "acme chair no 7",
+          price: 30000
+        }, Repo)
     end
-  
+
+
     @docp """
-      If you have a product with an external_product_id that matches their id and
-      it has the same name and the price is different, create a new past price record 
-      for your product. Then update the product's price. Do this even if the item is discontinued.
+      [ ] If you have a product with an external_product_id that matches their id and
+          it has the same name and the price is different, create a new past price record 
+          for your product. Then update the product's price. 
+          Do this even if the item is discontinued.
     """
     test "if the product exists, we attach the new price log and update the price" do
-      
+      {:ok, product_1 } = Transactor.merge_product(%{
+          company_code: "ACME",
+          external_product_id: "5323",
+          product_name: "acme chair no 5",
+          price: 50000
+      }, Repo)
+    
+      {:ok, product_2 } = Transactor.merge_product(%{
+          company_code: "ACME",
+          external_product_id: "5323",
+          product_name: "acme chair no 5",
+          price: 20000
+      }, Repo)
+    
+      # product ids should be the same
+      assert product_1.id == product_2.id
+
+      # it should now have two records
+      assert Enum.count(product_2.past_price_records) == 2
     end
-
-    @docp """
-      if you have a product record with a matching external_product_id 
-      but a different product name, log an error message that warns the team that 
-      there is a mismatch. Do not update the price.
-    """
-    test "" do
-
-    end
-
 
   end
 
   describe "Transactor.merge_products" do
-
+    
 
   end
 
